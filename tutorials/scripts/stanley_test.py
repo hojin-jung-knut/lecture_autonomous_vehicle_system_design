f446e25c178b7e1a96d1b65ba1f7ea3cc21e9938
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-04-11 12:29:50 +0900

    Update stanley_test.py

--- tutorials/scripts/stanley_test.py
+++ tutorials/scripts/stanley_test.py
@@ -8,6 +8,26 @@ from nav_msgs.msg import Odometry, Path
 from morai_msgs.msg import CtrlCmd, EgoVehicleStatus
 from tf.transformations import euler_from_quaternion
 
+class PIDControl:
+    def __init__(self, p_gain=0.3, i_gain=0.07, d_gain=0.03, control_time=0.02):
+        self.p_gain = p_gain
+        self.i_gain = i_gain
+        self.d_gain = d_gain
+        self.control_time = control_time
+        self.prev_error = self.i_control = 0
+
+    def pid(self, target_vel, current_vel):
+        error = target_vel - current_vel
+        p_control = self.p_gain * error
+
+        if abs(error) <= 5:
+            self.i_control += self.i_gain * error * self.control_time
+
+        d_control = self.d_gain * (error - self.prev_error) / self.control_time
+        self.prev_error = error
+
+        return p_control + self.i_control + d_control
+
 class Stanley:
     def __init__(self):
         rospy.init_node('stanley', anonymous=True)
@@ -16,7 +36,7 @@ class Stanley:
         rospy.Subscriber("/Ego_topic", EgoVehicleStatus, self.status_callback)
         self.ctrl_cmd_pub = rospy.Publisher('/ctrl_cmd', CtrlCmd, queue_size=1)
         self.ctrl_cmd_msg = CtrlCmd()
-        self.ctrl_cmd_msg.longlCmdType = 2
+        self.ctrl_cmd_msg.longlCmdType = 1 # Acceleration control type
 
         self.is_path = self.is_odom = self.is_status = False
         self.current_position = Point()
@@ -25,18 +45,21 @@ class Stanley:
         self.k = rospy.get_param('~stanley_gain', 0.8)
         self.v_t = rospy.get_param('~velocity_gain', 1.0)
 
-    def main(self):
-        rate = rospy.Rate(5) # 5Hz
+        self.pid_controller = PIDControl()
+        self.control_loop()
+    
+    def control_loop(self):
+        rate = rospy.Rate(15)
         while not rospy.is_shutdown():
             if self.is_path and self.is_odom and self.is_status:
-                self.process_control()
+                self.stanley_control()
             else:
-                self.log_missing_topics()
+                self.log_missing_data()
 
-            self.reset_topic_flags()
+            self.reset_flags()
             rate.sleep()
 
-    def process_control(self):
+    def stanley_control(self):
         vehicle_position = self.current_position
         self.is_look_forward_point = False
         translation_matrix = np.array([
@@ -69,12 +92,12 @@ class Stanley:
                 temp[1][1] = local_path_point[1]
 
         if self.is_look_forward_point:
-            self.calculate_steering(temp, temp_global, vehicle_position)
+            self.publish_control_commands(temp, temp_global, vehicle_position)
         else:
-            self.ctrl_cmd_msg.steering = self.ctrl_cmd_msg.velocity = 0.0
+            self.ctrl_cmd_msg.steering = self.ctrl_cmd_msg.accel = self.ctrl_cmd_msg.brake = 0.0
             rospy.logwarn("Forward point not found")
 
-    def calculate_steering(self, temp, temp_global, vehicle_position):
+    def publish_control_commands(self, temp, temp_global, vehicle_position):
         heading_error = atan2(temp[1][1] - temp[0][1], temp[1][0] - temp[0][0])
         cte = sin(self.vehicle_yaw) * (temp_global[0] - vehicle_position.x) - cos(self.vehicle_yaw) * (temp_global[1] - vehicle_position.y)
         crosstrack_error = -atan2(self.k * cte, self.current_vel + self.v_t)
@@ -82,8 +105,10 @@ class Stanley:
         steering_angle = heading_error + crosstrack_error
         steering_angle = np.clip(steering_angle, -pi/6, pi/6)
         self.ctrl_cmd_msg.steering = steering_angle
-        self.ctrl_cmd_msg.velocity = self.target_vel
-        rospy.loginfo(f"Steering (deg): {self.ctrl_cmd_msg.steering * 180 / pi:.2f}, Velocity (kph): {self.ctrl_cmd_msg.velocity:.2f}")
+        output = self.pid_controller.pid(self.target_vel, self.current_vel * 3.6)
+        self.ctrl_cmd_msg.accel = max(0, output)
+        self.ctrl_cmd_msg.brake = max(0, -output)
+        rospy.loginfo(f"Steering (deg): {self.ctrl_cmd_msg.steering * 180 / pi:.2f}, Accel: {min(100,self.ctrl_cmd_msg.accel):.2f}, Brake: {min(100,self.ctrl_cmd_msg.brake):.2f}")
         self.ctrl_cmd_pub.publish(self.ctrl_cmd_msg)
 
     def log_missing_topics(self):
