c9ee0592959790c5af4253de65ee7d0508d7e053
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-04-11 12:13:21 +0900

    Update pure_pursuit_test.py

--- tutorials/scripts/pure_pursuit_test.py
+++ tutorials/scripts/pure_pursuit_test.py
@@ -3,84 +3,129 @@
 
 import rospy, time
 import numpy as np
-from math import cos, sin, sqrt, atan2, pi
+from math import cos, sin, sqrt, atan2
 from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry, Path
-from morai_msgs.msg import CtrlCmd
+from morai_msgs.msg import CtrlCmd, EgoVehicleStatus
 from tf.transformations import euler_from_quaternion
+
+class PIDControl:
+    def __init__(self, p_gain=0.3, i_gain=0.07, d_gain=0.03, control_time=0.02):
+        self.p_gain = p_gain
+        self.i_gain = i_gain
+        self.d_gain = d_gain
+        self.control_time = control_time
+        self.prev_error = self.i_control = 0
+
+    def pid(self, target_vel, current_vel):
+        error = target_vel - current_vel
+        p_control = self.p_gain * error
+
+        if abs(error) <= 5:
+            self.i_control += self.i_gain * error * self.control_time
+
+        d_control = self.d_gain * (error - self.prev_error) / self.control_time
+        self.prev_error = error
+
+        return p_control + self.i_control + d_control
 
 class PurePursuit:
     def __init__(self):
-        rospy.init_node('pure_pursuit', anonymous=True)
-
-        self.local_path_sub = rospy.Subscriber("/local_path", Path, self.path_callback)
-        self.odom_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
+        rospy.init_node('pure_pursuit', anonymous=True)   
+        rospy.Subscriber("/local_path", Path, self.path_callback)
+        rospy.Subscriber("/odom", Odometry, self.odom_callback)
+        rospy.Subscriber("/Ego_topic", EgoVehicleStatus, self.status_callback)
         self.ctrl_cmd_pub = rospy.Publisher('/ctrl_cmd', CtrlCmd, queue_size=1)
 
-        self.ctrl_cmd_msg = CtrlCmd()
-        self.ctrl_cmd_msg.longlCmdType = 2 # Velocity control type
-
-        self.is_path = self.is_odom = False
+        self.is_path = self.is_odom = self.is_status = False
+        self.target_vel = 40.0 # Target velocity in km/h
+        self.current_vel = 0.0
         self.forward_point = self.current_position = Point()
-
+        self.is_look_forward_point = False
         self.vehicle_length = 3.0
-        self.lfd = 5.0
-
+        self.lfd = 20.0 # Look-ahead distance
         self.log_start_time = time.time()
-        self.last_log_time = 0.0
 
-    def main(self):
-        rate = rospy.Rate(30)
+        self.pid_controller = PIDControl()
+        self.control_loop()
 
+    def control_loop(self):
+        rate = rospy.Rate(15)
         while not rospy.is_shutdown():
-            if self.is_path and self.is_odom:
+            if self.is_path and self.is_odom and self.is_status:
                 self.pure_pursuit_control()
             else:
-                if not self.is_path:
-                    rospy.loginfo("Unable to subscribe to '/local_path' topic")
-                if not self.is_odom:
-                    rospy.loginfo("Unable to subscribe to '/odom' topic")
+                self.log_missing_data()
 
+            self.reset_flags()
             rate.sleep()
 
     def pure_pursuit_control(self):
-        vehicle_position = self.current_position
-        cos_yaw = cos(self.vehicle_yaw)
-        sin_yaw = sin(self.vehicle_yaw)
-        translation_matrix = np.array([
-            [cos_yaw, -sin_yaw, vehicle_position.x],
-            [sin_yaw, cos_yaw, vehicle_position.y],
-            [0, 0, 1]
-        ])
-        inv_translation_matrix = np.linalg.inv(translation_matrix)
+        translation = [self.current_position.x, self.current_position.y]
+        transformation_matrix = self.get_transformation_matrix(translation)
+        inv_transformation_matrix = np.linalg.inv(transformation_matrix)
 
         self.is_look_forward_point = False
-        for path_point in self.path.poses:
-            global_path_point = np.array([path_point.pose.position.x, path_point.pose.position.y, 1])
-            local_path_point = inv_translation_matrix.dot(global_path_point)
+        for pose in self.path.poses:
+            local_path_point = self.transform_point(inv_transformation_matrix, pose.pose.position)
 
             if local_path_point[0] > 0:
                 distance = sqrt(local_path_point[0]**2 + local_path_point[1]**2)
                 if distance >= self.lfd:
-                    self.forward_point = path_point.pose.position
+                    self.forward_point = pose.pose.position
                     self.is_look_forward_point = True
                     break
 
         if self.is_look_forward_point:
-            theta = atan2(local_path_point[1], local_path_point[0])
-            self.ctrl_cmd_msg.steering = atan2((2 * sin(theta) * self.vehicle_length), self.lfd)
-            self.ctrl_cmd_msg.velocity = 20.0
-
-            rospy.loginfo(f'''
-                -------------------------------------
-                "steering (deg) = {self.ctrl_cmd_msg.steering * 180 / pi:.2f}
-                "velocity (kph) = {self.ctrl_cmd_msg.velocity:.2f}
-                -------------------------------------''')   
+            self.publish_control_commands(local_path_point)
         else:
+            rospy.logwarn("No forward point found")
+            self.publish_control_commands([0, 0, 0], err=True)
+
+    def publish_control_commands(self, local_path_point, err=False):
+        self.ctrl_cmd_msg = CtrlCmd()
+        self.ctrl_cmd_msg.longlCmdType = 1 # Acceleration control type
+
+        if err:
             self.ctrl_cmd_msg.steering = self.ctrl_cmd_msg.velocity = 0.0
-            print("Forward point not found")
+        else:
+            theta = atan2(local_path_point[1], local_path_point[0])
+            self.ctrl_cmd_msg.steering = atan2(2 * self.vehicle_length * sin(theta), self.lfd)
+            output = self.pid_controller.pid(self.target_vel, self.current_vel * 3.6)
+
+            self.ctrl_cmd_msg.accel = max(0, output)
+            self.ctrl_cmd_msg.brake = max(0, -output)
+
+            rospy.loginfo(f"Accel: {min(100,self.ctrl_cmd_msg.accel):.2f}%, Brake: {min(100,self.ctrl_cmd_msg.brake):.2f}%")
 
         self.ctrl_cmd_pub.publish(self.ctrl_cmd_msg)
+
+    def transform_point(self, transformation_matrix, point):
+        global_path_point = np.array([point.x, point.y, 1])
+        local_path_point = transformation_matrix.dot(global_path_point)
+        return local_path_point
+
+    def get_transformation_matrix(self, translation):
+        return np.array([
+            [cos(self.vehicle_yaw), -sin(self.vehicle_yaw), translation[0]],
+            [sin(self.vehicle_yaw), cos(self.vehicle_yaw), translation[1]],
+            [0, 0, 1]
+        ])
+
+    def reset_flags(self):
+        self.is_path = self.is_odom = self.is_status = False
+
+    def log_missing_data(self):
+        if not self.is_path:
+            rospy.logwarn("Missing '/local_path' topic")
+        if not self.is_odom:
+            rospy.logwarn("Missing '/odom' topic")
+        if not self.is_status:
+            rospy.logwarn("Missing '/Ego_topic' topic")
+
+    def status_callback(self, msg):
+        self.is_status = True
+        self.current_vel = msg.velocity.x
 
     def path_callback(self, msg):
         self.is_path = True
@@ -88,14 +133,12 @@ class PurePursuit:
 
     def odom_callback(self, msg):
         self.is_odom = True
-        odom_quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
-                           msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+        odom_quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
         _, _, self.vehicle_yaw = euler_from_quaternion(odom_quaternion)
         self.current_position = msg.pose.pose.position
 
 if __name__ == '__main__':
     try:
         pure_pursuit = PurePursuit()
-        pure_pursuit.main()
     except rospy.ROSInterruptException:
         pass
