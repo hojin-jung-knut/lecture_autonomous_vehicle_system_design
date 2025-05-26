c75355bf987d59cdb061682f0c0d0cc0c12b4c99
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-05-26 15:31:06 +0900

    Update pedes_detector.py

--- tutorials/scripts/pedes_detector.py
+++ tutorials/scripts/pedes_detector.py
@@ -1,91 +1,70 @@
 #!/usr/bin/env python3
-#-*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
-import rospy
-import cv2
-import numpy as np
+import rospy, cv2, numpy as np
 from sensor_msgs.msg import CompressedImage
 
 def non_maximum_supression(bboxes, threshold=0.3):
-    
-    bboxes = sorted(bboxes, key=lambda detections: detections[3],
-            reverse=True)
-    new_bboxes=[]
-    
-    new_bboxes.append(bboxes[0])
-    
-    bboxes.pop(0)
+    if len(bboxes) == 0:
+        return []
 
-    for _, bbox in enumerate(bboxes):
+    bboxes = sorted(bboxes, key=lambda b: b[2] * b[3], reverse=True)
+    selected_bboxes = []
 
-        for new_bbox in new_bboxes:
+    while bboxes:
+        chosen = bboxes.pop(0)
+        selected_bboxes.append(chosen)
+        bboxes = [box for box in bboxes if compute_iou(chosen, box) < threshold]
 
-            x1_tl = bbox[0]
-            x2_tl = new_bbox[0]
-            x1_br = bbox[0] + bbox[2]
-            x2_br = new_bbox[0] + new_bbox[2]
-            y1_tl = bbox[1]
-            y2_tl = new_bbox[1]
-            y1_br = bbox[1] + bbox[3]
-            y2_br = new_bbox[1] + new_bbox[3]
-            
-            x_overlap = max(0, min(x1_br, x2_br)-max(x1_tl, x2_tl))
-            y_overlap = max(0, min(y1_br, y2_br)-max(y1_tl, y2_tl))
-            overlap_area = x_overlap * y_overlap
-            
-            area_1 = bbox[2] * new_bbox[3]
-            area_2 = new_bbox[2] * new_bbox[3]
-            
-            total_area = area_1 + area_2 - overlap_area
+    return selected_bboxes
 
-            overlap_area = overlap_area / float(total_area)
+def compute_iou(box1, box2):
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[0]+box1[2], box2[0]+box2[2])
+    y2 = min(box1[1]+box1[3], box2[1]+box2[3])
 
-            if overlap_area < threshold:
-                
-                new_bboxes.append(bbox)
+    inter_area = max(0, x2 - x1) * max(0, y2 - y1)
+    box1_area = box1[2] * box1[3]
+    box2_area = box2[2] * box2[3]
+    union_area = box1_area + box2_area - inter_area
 
-    return new_bboxes
-
-
+    return inter_area / union_area if union_area != 0 else 0
 
 class PEDESDetector:
     def __init__(self):
-   
-        self.rate = rospy.Rate(20)
-    
+        rospy.init_node('pedes_detector', anonymous=True)
         self.image_sub = rospy.Subscriber("/image_jpeg/compressed", CompressedImage, self.callback)
-
-        self.pedes_detector = cv2.HOGDescriptor()                              
+        self.pedes_detector = cv2.HOGDescriptor()
         self.pedes_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-        
+        rospy.on_shutdown(self.cleanup)
 
     def callback(self, msg):
-
-        self.rate.sleep()
-
         np_arr = np.frombuffer(msg.data, np.uint8)
         img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
-        (rects_temp, _) = self.pedes_detector.detectMultiScale(img_gray, winStride=(4, 4), padding=(8, 8), scale=8)
-        
+        (rects_temp, _) = self.pedes_detector.detectMultiScale(
+            img_gray,
+            winStride=(4, 4),
+            padding=(8, 8),
+            scale=1.05
+        )
+
         if len(rects_temp) != 0:
-            
             rects = non_maximum_supression(rects_temp)
+            for (x, y, w, h) in rects:
+                cv2.rectangle(img_bgr, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            for (x,y,w,h) in rects:
+        cv2.imshow("Pedestrian Detection", img_bgr)
+        cv2.waitKey(1)
 
-                cv2.rectangle(img_bgr, (x,y),(x+w,y+h),(0,255,255), 2)
-
-        cv2.imshow("Image window", img_bgr)
-        cv2.waitKey(1) 
-
+    def cleanup(self):
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-
-    rospy.init_node('pedes_detector', anonymous=True)
-
-    pedes_detector = PEDESDetector()
-
-    rospy.spin() 
+    try:
+        detector = PEDESDetector()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
