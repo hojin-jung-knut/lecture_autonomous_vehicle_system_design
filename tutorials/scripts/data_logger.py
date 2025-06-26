4fd3e64f22eea53f1c8d97a24a711b0c0bc1f382
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-06-26 14:52:46 +0900

    Update data_logger.py

--- tutorials/scripts/data_logger.py
+++ tutorials/scripts/data_logger.py
@@ -13,7 +13,7 @@ class DataLogger:
         rospy.init_node('data_logger', anonymous=True)
         rospy.Subscriber("/odom", Odometry, self.odom_callback)
         rospy.Subscriber("/local_path", Path, self.path_callback)
-        self.is_odom = False
+        self.is_path = self.is_odom = False
         self.forward_point = self.pose = Point()
         self.log_start_time = time.time()
         rospack = rospkg.RosPack()
@@ -22,17 +22,16 @@ class DataLogger:
         self.csv_file = open(self.log_file_path, mode='w', newline='')
         self.csv_writer = csv.writer(self.csv_file)
         self.csv_writer.writerow(['time', 'pos_x', 'pos_xd', 'pos_y', 'pos_yd'])
-        self.last_pose_x = 0.0
-        self.last_pose_y = 0.0
+        self.last_pose_x, self.last_pose_y = 0.0, 0.0
         self.lfd = 20.0 # Look-ahead distance
 
     def main(self):
         rate = rospy.Rate(30)
         while not rospy.is_shutdown():
-            if self.is_odom:
+            if self.is_odom and self.is_path:
                 self.forward_path()
             else:
-                rospy.loginfo("Unable to subscribe to '/odom' topic")
+                rospy.loginfo("Waiting for odometry and path data...")
             rate.sleep()
 
     def forward_path(self):
@@ -72,8 +71,8 @@ class DataLogger:
         self.pose = msg.pose.pose.position
         odom_quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
         _, _, self.vehicle_yaw = euler_from_quaternion(odom_quaternion)
-        if (self.last_pose_x - self.pose.x)**2 + (self.last_pose_y - self.pose.y)**2 > 1:
-            print(f"x: {self.pose.x:.2f}, y: {self.pose.y:.2f}")
+        if self.is_look_forward_point and (self.last_pose_x - self.pose.x)**2 + (self.last_pose_y - self.pose.y)**2 > 1:
+            # print(f"x: {self.pose.x:.2f}, y: {self.pose.y:.2f}")
             log = np.around([time.time() - self.log_start_time, self.pose.x,
                              self.forward_point.x, self.pose.y, self.forward_point.y], 4)
             self.csv_writer.writerow(log)
@@ -87,9 +86,12 @@ class DataLogger:
 
 if __name__ == '__main__':
     try:
-        data_logger = DataLogger("tutorials", "term_project_studentnumber")
+        data_logger = DataLogger("tutorials", "log")
         data_logger.main()
     except rospy.ROSInterruptException:
         pass
     finally:
-        data_logger.stop()
+        try:
+            data_logger.stop()
+        except NameError:
+            pass
