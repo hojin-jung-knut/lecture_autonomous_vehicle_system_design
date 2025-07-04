41ebf0f30035b91cb51928dc632b5789b7283b6d
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-07-04 10:48:14 +0900

    Update turtle_status.py

--- tutorials/scripts/turtle_status.py
+++ tutorials/scripts/turtle_status.py
@@ -9,24 +9,24 @@ class turtle_listener():
     def __init__(self):
         rospy.init_node('status_listener', anonymous=True)
         # /turtle1/pose 구독
-        rospy.Subscriber('/turtle1/pose', Pose, self.statusCB)
-        self.status_msg=Pose()
+        rospy.Subscriber('/turtle1/pose', Pose, self.callback)
+        self.status_msg = Pose()
         rospy.spin()
 
-    def statusCB(self,data): ## turtle Status subscriber
-        self.status_msg=data
+    def callback(self,data): ## turtle Status subscriber
+        self.status_msg = data
         print("tf broad cast")
         # 브로드캐스터 생성
         br = tf.TransformBroadcaster()
         # turtle1 상태 tf 브로드캐스팅
         br.sendTransform((self.status_msg.x, self.status_msg.y, 0),
-                        tf.transformations.quaternion_from_euler(0,0, self.status_msg.theta),
+                        tf.transformations.quaternion_from_euler(0, 0, self.status_msg.theta),
                         rospy.Time.now(),
                         "turtle",
                         "map")
 
 if __name__ == '__main__':
     try:
-        tl=turtle_listener()
+        tl = turtle_listener()
     except rospy.ROSInternalException:
         pass
