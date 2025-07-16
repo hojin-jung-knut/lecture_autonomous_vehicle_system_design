2b6e47214fdb7fe123a7f5b1018da656a6f90e3a
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-07-15 18:44:23 -0700

    Update controller_pure_pursuit_gear.py

--- tutorials/scripts/controller_pure_pursuit_gear.py
+++ tutorials/scripts/controller_pure_pursuit_gear.py
@@ -3,60 +3,59 @@
 
 import rospy, numpy as np
 import path_curvature
-from math import cos, sin, sqrt, atan2
+from math import cos, sin, sqrt, atan2, pi
 from geometry_msgs.msg import Point
 from nav_msgs.msg import Odometry, Path
 from morai_msgs.msg import CtrlCmd, EgoVehicleStatus, EventInfo
 from tf.transformations import euler_from_quaternion
 from morai_msgs.srv import MoraiEventCmdSrv
 from enum import Enum
+import time
 
 class Gear(Enum):
-	P = 1
-	R = 2
-	N = 3
-	D = 4
+	P, R, N, D = 1, 2, 3, 4
 
 
 class PIDControl:
-    def __init__(self, p_gain=0.3, i_gain=0.03, d_gain=0.003, control_time=0.01):
+    def __init__(self, p_gain=0.1, i_gain=0.001, d_gain=0.003, control_time=0.01):
         self.p_gain = p_gain
         self.i_gain = i_gain
         self.d_gain = d_gain
         self.control_time = control_time
-        self.prev_error = self.i_control = 0
+        self.prev_error, self.i_control = 0, 0
+        self.t = 0
 
     def pid(self, target_vel, current_vel):
         error = target_vel - current_vel
         p_control = self.p_gain * error
-
-        if abs(error) <= 5:
-            self.i_control += self.i_gain * error * self.control_time
-
+        if abs(error) <= 5: self.i_control += self.i_gain * error * self.control_time
         d_control = self.d_gain * (error - self.prev_error) / self.control_time
         self.prev_error = error
-
+        t = self.t
+        self.t = time.time()
+        # self.control_time = min(1, self.t - t)
+        rospy.loginfo("p: %f, i: %f, d: %f", p_control, self.i_control, d_control)
         return p_control + self.i_control + d_control
+
 
 class PurePursuit:
     def __init__(self):
-        rospy.init_node('pure_pursuit', anonymous=True)   
+        rospy.init_node('pure_pursuit', anonymous=True)
         rospy.Subscriber("/local_path", Path, self.path_callback)
         rospy.Subscriber("/odom", Odometry, self.odom_callback)
         rospy.Subscriber("/Ego_topic", EgoVehicleStatus, self.status_callback)
         self.ctrl_cmd_pub = rospy.Publisher('/ctrl_cmd_0', CtrlCmd, queue_size=1) # check topic in MORAI Sim (/ctrl_cmd_0)
         
         self.is_path = self.is_odom = self.is_status = False
-        self.is_gear = False
         self.ctrl_cmd_msg = CtrlCmd()
         self.ctrl_cmd_msg.longlCmdType = 1
-        self.target_vel = 60.0  # in m/s
+        self.target_vel = 30.0 # km/h
         self.current_vel = 0.0
-        self.forward_point = self.current_position = Point()
+        self.forward_point, self.current_position = Point(), Point()
         self.is_look_forward_point = False
         self.vehicle_length = 3.0
         self.lfd = 5.0 # Look-ahead distance
-        self.pid_controller = PIDControl()
+        self.accel_brake = PIDControl()
         self.curv = path_curvature.Curvature()
         rospy.wait_for_service('/Service_MoraiEventCmd')
         self.event_cmd_srv = rospy.ServiceProxy('Service_MoraiEventCmd', MoraiEventCmdSrv)
@@ -64,33 +63,28 @@ class PurePursuit:
         self.control_loop()
 
     def control_loop(self):
-        self.rate = rospy.Rate(10)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.is_path and self.is_odom and self.is_status:
                 path_count = len(np.array(self.path.poses)) - 5
-                # if path_count < 5: self.target_vel = 0
-                if path_count > 5:
+                if path_count > 6:
                     curvature = self.curv.path_curvature(self.path, self.current_position, path_count)
-                print(path_count)
                 if path_count < 7 :
                     print("DONE!")
                     self.send_gear_cmd(Gear.P.value)
                     break
                 if curvature <= 200:
-                # print(sqrt(curvature * 9.81 * 1))
-                    self.target_vel = sqrt(curvature * 9.81 * 1)
                     if 50 < curvature < 200:
-                        self.target_vel = 40.0
-                    if curvature < 40 :
-                        self.target_vel  = 10.0
-                else: self.target_vel = 60.0
-                print("curvature / self.target_vel", curvature, self.target_vel)
-                rospy.logwarn("curvature :%d", curvature)
+                        self.target_vel = 10.0
+                    if curvature < 51 :
+                        self.target_vel = 20.0
+                else: self.target_vel = 30.0
+                rospy.loginfo("curvature(m): %d, self.target_vel(km/h): %d", curvature, self.target_vel)
                 self.pure_pursuit_control()
             else:
                 self.missing_topics()
             self.reset_flags()
-            self.rate.sleep()
+            rate.sleep()
 
     def pure_pursuit_control(self):
         translation = [self.current_position.x, self.current_position.y]
@@ -116,18 +110,20 @@ class PurePursuit:
 
     def publish_control_commands(self, local_path_point, err=False):
         if err:
-            self.ctrl_cmd_msg.steering = self.ctrl_cmd_msg.velocity = 0.0
+            self.ctrl_cmd_msg.steering = 0.0
+            self.ctrl_cmd_msg.accel = 0.0
+            self.ctrl_cmd_msg.brake = 0.1
         else:
             theta = atan2(local_path_point[1], local_path_point[0])
             self.lfd = self.current_vel * 0.7
             if self.lfd < 5: self.lfd = 5
             self.ctrl_cmd_msg.steering = atan2(2 * self.vehicle_length * sin(theta), self.lfd)
-            output = self.pid_controller.pid(self.target_vel, self.current_vel * 3.6)
+            output = self.accel_brake.pid(self.target_vel, self.current_vel * 3.6)
 
-            self.ctrl_cmd_msg.accel = max(0, output)
-            self.ctrl_cmd_msg.brake = max(0, -output)
+            self.ctrl_cmd_msg.accel = min(1, max(output, 0))
+            self.ctrl_cmd_msg.brake = min(1, max(-output, 0))
 
-            rospy.loginfo(f"Accel: {min(100,self.ctrl_cmd_msg.accel):.2f}%, Brake: {min(100,self.ctrl_cmd_msg.brake):.2f}%")
+            rospy.loginfo(f"Accel: {self.ctrl_cmd_msg.accel*100:.2f}%, Brake: {self.ctrl_cmd_msg.brake*100:.2f}%, Steer(deg): {self.ctrl_cmd_msg.steering*180/pi:.2f}")
 
         self.ctrl_cmd_pub.publish(self.ctrl_cmd_msg)
 
@@ -178,11 +174,11 @@ class PurePursuit:
         gear_cmd.option = 3
         gear_cmd.ctrl_mode = 3
         gear_cmd.gear = gear_mode
-        resp = self.event_cmd_srv(gear_cmd)
-        rospy.loginfo(resp)    
-        
+        self.event_cmd_srv(gear_cmd)
+
+
 if __name__ == '__main__':
     try:
-        pure_pursuit = PurePursuit()
+        PurePursuit()
     except rospy.ROSInterruptException:
         pass
