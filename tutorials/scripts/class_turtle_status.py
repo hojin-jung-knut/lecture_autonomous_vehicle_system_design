b965c3d9a6897c31d7ce83fd85c6f025e33f74d5
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-07-23 15:43:06 +0900

    Update class_turtle_status.py

--- tutorials/scripts/class_turtle_status.py
+++ tutorials/scripts/class_turtle_status.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import rospy
-import tf
-# /turtle1/pose 토픽 타입인 turtlesim/Pose import
+
+import rospy, tf
 from turtlesim.msg import Pose
 
-class turtle_listener():
+class TFBroadCaster():
     def __init__(self):
-        rospy.init_node('status_listener', anonymous=True)
-        # /turtle1/pose 구독
-        rospy.Subscriber('/turtle1/pose', Pose, self.callback)
+        rospy.init_node('broadcaster', anonymous=True)
+        rospy.Subscriber('turtle1/pose', Pose, self.callback)
         self.status_msg = Pose()
+        self.isbroadcast = False
         rospy.spin()
 
-    def callback(self,data): ## turtle Status subscriber
+    def callback(self, data):
         self.status_msg = data
-        print("tf broad cast")
+        if not self.isbroadcast:
+            rospy.loginfo("Broadcasting TF..")
+            self.isbroadcast = True
         # 브로드캐스터 생성
         br = tf.TransformBroadcaster()
         # turtle1 상태 tf 브로드캐스팅
         br.sendTransform((self.status_msg.x, self.status_msg.y, 0),
-                        tf.transformations.quaternion_from_euler(0, 0, self.status_msg.theta),
+                        tf.transformations.quaternion_from_euler(0,0,self.status_msg.theta),
                         rospy.Time.now(),
                         "turtle",
                         "map")
 
 if __name__ == '__main__':
     try:
-        tl = turtle_listener()
+        TFBroadCaster()
     except rospy.ROSInternalException:
         pass
