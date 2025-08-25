72bceb09c6157b5d4fe6d57a0cf500a29cd5279a
hojin.jung <61054354+hojin-jung-knut@users.noreply.github.com>
2025-08-24 18:35:34 -0700

    Update data_logger.py

--- tutorials/scripts/data_logger.py
+++ tutorials/scripts/data_logger.py
@@ -16,7 +16,7 @@ class DataLogger:
         rospy.Subscriber("/global_path", Path, self.global_path_callback)
         rospy.Subscriber("/local_path", Path, self.path_callback)
         rospy.Subscriber("/Ego_topic", EgoVehicleStatus, self.status_callback)
-        self.is_path = self.is_odom = self.is_status = False
+        self.is_global_path = self.is_path = self.is_odom = self.is_status = False
         self.ref_point, self.pose = Point(), Point()
         self.log_start_time = time.time()
         rospack = rospkg.RosPack()
@@ -28,12 +28,9 @@ class DataLogger:
         self.last_pose_x, self.last_pose_y = 0.0, 0.0
 
     def main(self):
-        rate = rospy.Rate(5)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            if not hasattr(self, 'ego'):
-                rate.sleep()
-                continue
-            if self.is_odom and self.is_path and self.is_status:
+            if self.is_global_path and self.is_path and self.is_odom and self.is_status:
                 self.closest_points()
                 log = np.around([time.time() - self.log_start_time, self.pose.x, self.ref_point.x, self.pose.y, 
                     self.ref_point.y, self.ego.velocity.x*3.6, self.ego.wheel_angle, self.ego.accel, self.ego.brake], 4)
@@ -45,7 +42,7 @@ class DataLogger:
             else:
                 rospy.loginfo("Waiting for odometry and path data...")
             
-            self.reset_flags()
+            #self.reset_flags()
             rate.sleep()
     
     def closest_points(self):
@@ -57,7 +54,7 @@ class DataLogger:
                 self.ref_point = pose.pose.position
 
     def global_path_callback(self, msg):
-        self.is_path = True
+        self.is_global_path = True
         self.global_path = msg
 
     def path_callback(self, msg):
